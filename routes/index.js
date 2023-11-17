c7daed12ec968fa78cafed65dc8646b48ca9203a
hashilyze <43923425+hashilyze@users.noreply.github.com>
2023-11-17 19:24:10 +0900

    ADD: HomePage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', async function (req, res) {
+    let bag = { }
+    try{
+        if(req.session.uid){
+            bag.isPrivate = true;
+            bag.uid = req.session.uid;
+            bag.user_name = (await User.findById(bag.uid)).name;
+
+            res.render('home', bag);
+            return;
+        }
+    } catch(e){
+        console.log(e);
+    }
+    bag.isPrivate = false;
+    res.render('home', bag);
+
+    //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
